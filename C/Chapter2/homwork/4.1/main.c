3b58fc8f0eb19e2859ee7ca5f5a5066fd2a16f0d
YYZYHS <77320657+YYZYHS@users.noreply.github.com>
2021-01-13 17:57:05 +0800

    第二章的题目：A，B互换

---
+++ C/Chapter2/homwork/4.1/main.c
@@ -0,0 +1,24 @@
+//题目要求：
+//A，B互换
+
+/*
+	伪代码
+	c = a
+	a = b
+	b = c
+*/
+
+//开始写代码
+
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("please press the value of a & b:");
+	scanf("%d,%d",&a,&b);
+	printf("Before:\na=%d\nb=%d\n",a,b);
+	c = a;
+	a = b;
+	b = c;
+	printf("Later:\na=%d\nb=%d\n",a,b);
+}
